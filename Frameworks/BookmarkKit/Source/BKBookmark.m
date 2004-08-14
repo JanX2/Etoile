4b3e32c32a260110222e04ce92dcf79b35c1dbe8
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2004-08-14 21:16:02 +0000

    Initial revision
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@11 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Frameworks/BookmarkKit/Source/BKBookmark.m
@@ -0,0 +1,101 @@
+/*
+	BKBookmark.m
+
+	BKBookmark is the BookmarkKit class which is used to represent a bookmark
+
+	Copyright (C) 2004 Quentin Mathe <qmathe@club-internet.fr>	                   
+
+	Author:  Quentin Mathe <qmathe@club-internet.fr>
+	Date:  April 2004
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#import <Foundation/Foundation.h>
+#import "BKBookmark.h"
+
+@implementation BKBookmark
+
++ bookmarkWithURL: (NSURL *)url
+{
+
+}
+
++ bookmarkWithXBEL: (NSString *)xbel
+{
+
+}
+
+- (id) initWithURL: (NSURL *)url
+{
+
+}
+
+- (id) initWithXBEL: (NSString *)xbel
+{
+
+}
+
+- (NSURL *) URL
+{
+
+}
+
+- (NSDate *) creationDate
+{
+
+}
+
+- (void) setCreationDate: (NSDate *)date
+{
+
+}
+
+- (NSDate *) lastVisitDate
+{
+
+}
+
+- (void) setLastVisitDate: (NSDate *)date
+{
+
+}
+
+- (NSImage *) favIcon
+{
+
+}
+
+- (void) setFavIcon: (NSImage *)icon
+{
+
+}
+
+- (id) propertyForKey: (NSString *)key
+{
+
+}
+
+- (void) setTextProperty: (NSString *)text forKey: (NSString *)key
+{
+
+}
+
+- (void) setImageProperty: (NSImage *)image forKey: (NSString *)key
+{
+
+}
+
+@end
---
+++ Frameworks/BookmarkKit/Source/BKBookmarkStore.m
@@ -0,0 +1,99 @@
+/*
+	BKBookmarkStore.m
+
+	BKBookmarkStore is the core BookmarkKit class to interact with the bookmarks
+
+	Copyright (C) 2004 Quentin Mathe <qmathe@club-internet.fr>	                   
+
+	Author:  Quentin Mathe <qmathe@club-internet.fr>
+	Date:  April 2004
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#import <Foundation/Foundation.h>
+
+@implementation BKBookmarkStore
+
++ (BKBookmarkStore *) sharedInstanceForDefaultPath
+{
+
+}
+
++ (BKBookmarkStore *) sharedInstanceForPath: (NSString *)path 
+// support native format or XBEL format
+{
+
+}
+
++ (BKBookmarkStore *) sharedInstanceForURL: (NSURL *)url
+// support native format or XBEL format
+{
+
+}
+
+- (NSString *) path
+{
+
+}
+
+- (void) addProtocol: (BKBookmarkProtocol)bookmarkProtocol 
+  relativeToResourceSpecifier: (NSString *)resourceSpecifier 
+  relatedToProcotols: (BKBookmarkProtocol)bookmarkProtocol;
+{
+
+}
+
+- (void) removeProtocol: (BKBookmarkProtocol)bookmarkProtocol
+{
+
+}
+
+- (void) addBookmark: (BKBookmark *)bookmark
+{
+
+}
+
+- (void) removeBookmark: (BKBookmark *)bookmark
+{
+
+}
+
+- (BKBookmarkSearchResult *) searchWithQuery: (BKBookmarkQuery *)query
+{
+
+}
+
+- (void) save
+{
+
+}
+
+- (void) hasUnsavedChanges
+{
+
+}
+
+- (NSString *) transformToXBEL // aspect
+{
+
+}
+
+- (NSString *) transformToXMLNativeFormat // aspect
+{
+
+}
+
+@end
