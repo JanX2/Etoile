6799aaae155bcc3f97b87dc66f02076220238444
rio <rio@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2005-03-22 22:38:23 +0000

    Addition of NSColor.h
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@100 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Bundles/Camaelon/NSColor.h
@@ -0,0 +1,17 @@
+#import <AppKit/AppKit.h>
+#import "GraphicToolbox.h"
+
+@interface NSColor (theme)
++ (NSColor*) titlebarTextColor;
++ (NSColor*) selectedTitlebarTextColor;
++ (NSColor*) rowBackgroundColor;
++ (NSColor*) alternateRowBackgroundColor;
++ (NSColor*) rowTextColor;
++ (NSColor*) selectedRowBackgroundColor;
++ (NSColor*) selectedRowTextColor;
++ (NSColor*) selectedControlColor;
++ (NSColor*) selectedTextColor; 
++ (NSColor*) selectedTextBackgroundColor;
++ (NSColor*) windowBackgroundColor;
++ (NSColor*) controlBackgroundColor;
+@end
