458f006a05b82b9c83a435daaf371ac3a6f3b782
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2005-05-19 06:47:00 +0000

    Add TestQueryTermVectors.m
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@177 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Frameworks/LuceneKit/Source/Search/TestQueryTermVector.m
@@ -0,0 +1,47 @@
+#include <UnitKit/UnitKit.h>
+#include <Foundation/Foundation.h>
+#include <LuceneKit/Search/LCQueryTermVector.h>
+#include <LuceneKit/Analysis/LCWhitespaceAnalyzer.h>
+
+@interface TestQueryTermVector: NSObject <UKTest>
+@end
+
+@implementation TestQueryTermVector
+
+- (void) checkGold: (NSArray *) terms gold: (NSArray *) gold freqs: (NSArray *) freqs goldFreqs: (NSArray *) goldFreqs
+{
+	int i;
+	for (i = 0; i < [terms count]; i++)
+	{
+		UKObjectsEqual([terms objectAtIndex: i], [gold objectAtIndex: i]);
+		UKIntsEqual([[freqs objectAtIndex: i] intValue], [[goldFreqs objectAtIndex: i] intValue]);
+	}
+}
+
+- (void) testConstructor
+{
+	NSArray *queryTerm = [NSArray arrayWithObjects: @"foo", @"bar", @"foo", @"again", @"foo", @"bar",
+		@"go", @"go", @"go", nil];
+	NSArray *gold = [NSArray arrayWithObjects: @"again", @"bar", @"foo", @"go", nil];
+	NSArray *goldFreqs = [NSArray arrayWithObjects: @"1", @"2", @"3", @"3", nil];
+	LCQueryTermVector *result = [[LCQueryTermVector alloc] initWithQueryTerms: queryTerm];
+	UKNotNil(result);
+	NSArray *terms = [result terms];
+	UKIntsEqual(4, [terms count]);
+	NSArray *freqs = [result termFrequencies];
+	UKIntsEqual(4, [freqs count]);
+	[self checkGold: terms gold: gold freqs: freqs goldFreqs: goldFreqs];
+	result = [[LCQueryTermVector alloc] initWithQueryTerms: nil];
+	UKIntsEqual(0, [[result terms] count]);
+	
+	result = [[LCQueryTermVector alloc] initWithString: @"foo bar foo again foo bar go go go"
+											  analyzer: [[LCWhitespaceAnalyzer alloc] init]];
+	UKNotNil(result);
+	terms = [result terms];
+	UKIntsEqual(4, [terms count]);
+	freqs = [result termFrequencies];
+	UKIntsEqual(4, [freqs count]);
+	[self checkGold: terms gold: gold freqs: freqs goldFreqs: goldFreqs];
+}
+
+@end
