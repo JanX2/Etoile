3bc07f6fbd312bbf807719510357a0463b2900cb
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2005-08-01 05:40:35 +0000

    Add more tests
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@283 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Frameworks/OgreKit/Source/TestOGRegularExpression.m
+++ Frameworks/OgreKit/Source/TestOGRegularExpression.m
@@ -9,8 +9,6 @@
 - (void) testBasic
 {
   OGRegularExpression *regex = [OGRegularExpression regularExpressionWithString:@"a[^a]*a"];
-//  OGRegularExpression *regex = [OGRegularExpression regularExpressionWithString:@"gamma"];
-  NSEnumerator *enumerator = [regex matchEnumeratorInString:@"alphabetagammadelta"];
   NSArray *matches = [regex allMatchesInString:@"alphabetagammadelta"];
   UKIntsEqual(3, [matches count]);
 
@@ -21,4 +19,29 @@
   match = [matches objectAtIndex: 2];;
   UKStringsEqual(@"adelta", [match matchedString]);
 }
+
+- (void) testSubstring
+{
+  OGRegularExpression *regex = [OGRegularExpression regularExpressionWithString: @"(\\d+):(\\d+)"];
+  NSArray *matches = [regex allMatchesInString: @"Time: 12:34am"];
+  UKIntsEqual(1, [matches count]);
+  OGRegularExpressionMatch *match = [matches objectAtIndex: 0];
+  UKIntsEqual(0, [match index]);
+  UKIntsEqual(3, [match count]);
+  UKStringsEqual(@"12:34", [match substringAtIndex: 0]);
+  UKStringsEqual(@"12", [match substringAtIndex: 1]);
+  UKStringsEqual(@"34", [match substringAtIndex: 2]);
+  UKStringsEqual(@"Time: ", [match prematchString]);
+  UKStringsEqual(@"am", [match postmatchString]);
+}
+
+- (void) testReplacement
+{
+  /* Swap first and second substring */
+  OGRegularExpression *regex = [OGRegularExpression regularExpressionWithString: @"(\\d+):(\\d+)"];
+  NSString *replace = [regex replaceFirstMatchInString: @"Time: 12:34am"
+                      withString: @"\\2:\\1"];
+  UKStringsEqual(@"Time: 34:12am", replace);
+}
+
 @end
