e690cb168ac5a15f5027c9288f8d2869e6d7ddbc
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2005-08-14 09:35:59 +0000

    Added PreferencesKitExample GNUstep support
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@302 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Examples/PreferencesKitExample/SamplePrefsModule/SamplePrefsModule.h
+++ Developer/Examples/PreferencesKitExample/SamplePrefsModule/SamplePrefsModule.h
@@ -1,11 +1,14 @@
 #import <AppKit/NSNibDeclarations.h>
 #import <PreferencesKit/PrefsModule.h>
 
+
 @interface SamplePrefsModule: NSObject <PrefsModule>
 {
 	IBOutlet id				window;
 	IBOutlet id				view;
 	id <PrefsController>	controller;
 }
+
+- (void) initUI;
 
 @end
--- Developer/Examples/PreferencesKitExample/main.m
+++ Developer/Examples/PreferencesKitExample/main.m
@@ -6,7 +6,22 @@
 //  Copyright __MyCompanyName__ 2005. All rights reserved.
 //
 
+#ifdef GNUSTEP
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+// FIXME: Hack to workaround the fact Gorm doesn't support NSView as custom view
+// class.
+@interface CustomView : NSView { }
+@end
+
+@implementation CustomView
+@end
+
+#else
 #import <Cocoa/Cocoa.h>
+#endif
+
 #import <PreferencesKit/PreferencesKit.h>
 
 int main(int argc, char *argv[])
