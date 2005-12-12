a6e25771ea8d296d0b4aaba0e9698eadb77292b0
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2005-12-12 17:22:30 +0000

    Imported yet another one Preferences related applications called Hardware
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@385 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Services/User/Hardware/StartupPrefPane/StartupPrefPane.h
@@ -0,0 +1,34 @@
+/** <title>StartupPrefPane.h</title>
+
+    <abstract>Startup Settings related Preference Pane</abstract>
+
+    Copyright (C) 2005 Quentin Mathe <qmathe@club-internet.fr>
+
+    Author:  Quentin Mathe <qmathe@club-internet.fr>
+    Date:  December 2005
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program; see the file COPYING.LIB.
+    If not, write to the Free Software Foundation,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#import <PreferencesKit/PreferencesKit.h>
+
+
+@interface StartupPrefPane : NSPreferencePane
+{
+
+}
+
+@end
---
+++ Services/User/Hardware/main.m
@@ -0,0 +1,41 @@
+/*
+    main.m
+
+    Copyright (C) 2005 Quentin Mathe <qmathe@club-internet.fr>
+
+    Author:  Quentin Mathe <qmathe@club-internet.fr>
+    Date:  November 2005
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this program; see the file COPYING.LIB.
+    If not, write to the Free Software Foundation,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <PreferencesKit/PreferencesKit.h>
+
+// FIXME: Hack to workaround the fact Gorm doesn't support NSView as custom view
+// class.
+@interface CustomView : NSView { }
+@end
+
+@implementation CustomView
+@end
+
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
