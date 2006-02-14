579c6b5a1ef7c1d2875ff6ba00a41a5358e8c788
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2006-02-14 15:52:40 +0000

    Forgot to commit these two files with previous revision r517 (in PreferencesKit)
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@518 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Frameworks/PreferencesKit/Source/PKMatrixView.h
+++ Frameworks/PreferencesKit/Source/PKMatrixView.h
@@ -1,7 +1,29 @@
-#ifndef __PreferencesKit_PKMatrixView__
-#define __PreferencesKit_PKMatrixView__
+/*
+	PKMatrixView.h
+ 
+	Matrix view enclosed in a scroll view (with pane displayed in a content subview)
+ 
+	Copyright (C) 2005 Yen-Ju Chen, Quentin Mathe 
+ 
+	Author:  Yen-Ju Chen
+	Date:  December 2005
+ 
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+ 
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	Lesser General Public License for more details.
+ 
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
-#include <AppKit/AppKit.h>
+#import <AppKit/AppKit.h>
 
 @interface PKMatrixView: NSControl
 {
@@ -22,5 +44,3 @@
 - (NSView *) contentView;
 
 @end
-
-#endif /* __PreferencesKit_PKMatrixView__ */
