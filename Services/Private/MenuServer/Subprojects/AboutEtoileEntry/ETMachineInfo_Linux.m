bd2d51799c34642cd3e76392ec10c2ae32a9d356
saso <saso@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2006-05-16 06:32:18 +0000

    Applied changes to machine-info determining by David Chrisnall.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@822 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Services/Private/MenuServer/Subprojects/AboutEtoileEntry/ETMachineInfo_Linux.m
@@ -0,0 +1,98 @@
+/*
+    ETMachineInfo_Linux.m
+
+    Linux specific backend for ETMachineInfo.
+
+    Copyright (C) 2005, 2006  Saso Kiselkov
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifdef LINUX
+
+#import "ETMachineInfo.h"
+#import <sys/types.h>
+#import <sys/stat.h>
+
+#import <Foundation/NSArray.h>
+#import <Foundation/NSFileManager.h>
+#import <Foundation/NSString.h>
+
+@implementation ETMachineInfo (Linux)
+
++ (unsigned long long) realMemory
+{
+  // The size of /proc/kcore on Linux gives the total memory
+  return [[[NSFileManager defaultManager]
+    fileAttributesAtPath: @"/proc/kcore" traverseLink: YES]
+    fileSize];
+}
+
++ (unsigned int) cpuMHzSpeed
+{
+  // read /proc/cpuinfo for the processor type
+  NSEnumerator * e = [[[NSString stringWithContentsOfFile: @"/proc/cpuinfo"]
+    componentsSeparatedByString: @"\n"]
+    objectEnumerator];
+  NSString * line;
+
+  while ((line = [e nextObject]) != nil)
+    {
+      if ([line hasPrefix: @"cpu MHz"])
+        {
+           NSArray * comps = [line componentsSeparatedByString: @":"];
+
+           if ([comps count] > 1)
+             {
+               return [[comps objectAtIndex: 1] intValue];
+             }
+        }
+    }
+
+  return 0;
+}
+
++ (NSString *) cpuName
+{
+  // read /proc/cpuinfo for the processor type
+  NSEnumerator * e = [[[NSString stringWithContentsOfFile: @"/proc/cpuinfo"]
+    componentsSeparatedByString: @"\n"]
+    objectEnumerator];
+  NSString * line;
+
+  while ((line = [e nextObject]) != nil)
+    {
+      if ([line hasPrefix: @"model name"])
+        {
+          NSArray * comps = [line componentsSeparatedByString: @":"];
+
+          if ([comps count] > 1)
+            {
+              return [[comps objectAtIndex: 1] stringByTrimmingSpaces];
+            }
+        }
+    }
+
+  return nil;
+}
+
++ (BOOL) platformSupported
+{
+  return YES;
+}
+
+@end
+
+#endif // LINUX
---
+++ Services/Private/MenuServer/Subprojects/AboutEtoileEntry/ETMachineInfo_OpenBSD.m
@@ -0,0 +1,65 @@
+/*
+ * ETMachineInfo_OpenBSD.m - OpenBSD specific backend for ETMachineInfo
+ *
+ * Copyright 2006, David Chisnall
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef OPENBSD
+
+#import "ETMachineInfo.h"
+#import "TRSysctl.h"
+
+@implementation ETMachineInfo (OpenBSD)
+
++ (unsigned long long) realMemory
+{
+  int sysctlName[] = {CTL_HW, HW_PHYSMEM};
+
+  return (unsigned long long) performIntegerSysctl (sysctlName, 2);
+}
+
++ (unsigned int) cpuMHzSpeed
+{
+  int sysctlName[] = {CTL_HW, HW_CPUSPEED};
+
+  return performIntegerSysctl (sysctlName, 2);
+}
+
++ (NSString *) cpuName
+{
+  int sysctlName[] = {CTL_HW, HW_MODEL};
+
+  return performSysctl (sysctlName, 2);
+}
+
++ (BOOL) platformSupported
+{
+  return YES;
+}
+
+@end
+
+#endif // OPENBSD
