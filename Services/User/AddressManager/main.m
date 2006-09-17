f60db899ba443a058ceafaee5bc7c42967f4b7bf
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2006-09-17 08:03:40 +0000

    Initial import
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@971 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Services/User/AddressManager/main.m
@@ -0,0 +1,17 @@
+// main.m<2> (this is -*- ObjC -*-)
+// 
+// \author: Björn Giesler <giesler@ira.uka.de>
+// 
+// 
+// 
+// $Author: bjoern $
+// $Locker:  $
+// $Revision: 1.1.1.1 $
+// $Date: 2004/02/14 18:00:01 $
+
+#include <AppKit/AppKit.h>
+
+int main(int argc, const char** argv)
+{
+  return NSApplicationMain(argc, argv);
+}
