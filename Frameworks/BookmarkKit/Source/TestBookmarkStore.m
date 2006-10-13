faf09538a74923a0c4325dfa2c55a511b0515e0c
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2006-10-13 06:14:12 +0000

    Add save test
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@1008 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Frameworks/BookmarkKit/Source/TestBookmarkStore.m
+++ Frameworks/BookmarkKit/Source/TestBookmarkStore.m
@@ -1,4 +1,4 @@
-#import <BookmarkKit/BKBookmarkStore.h>
+#import <BookmarkKit/BookmarkKit.h>
 #import <Foundation/Foundation.h>
 #import <UnitKit/UnitKit.h>
 
@@ -11,4 +11,27 @@
   NSString *path = [[BKBookmarkStore sharedBookmarkStore] path];
   UKNotNil(path);
 }
+
+#if 0
+- (void) testSave
+{
+  NSString *path = @"/tmp/subdir/bookmark_test.bookmark";
+  BKBookmarkStore *store = [BKBookmarkStore sharedBookmarkAtPath: path];
+  BKBookmark *bk = [BKBookmark bookmarkWithURL: [NSURL URLWithString: @"http://www.gnustep.org"]];
+  [store addRecord: bk];
+  BKBookmark *bk1 = [BKBookmark bookmarkWithURL: [NSURL URLWithString: @"http://www.etoile-project.org"]];
+  [store addRecord: bk1];
+  BKBookmark *bk2 = [BKBookmark bookmarkWithURL: [NSURL URLWithString: @"http://www.google.org"]];
+  [store addRecord: bk2];
+
+  BKGroup *gp = [[BKGroup alloc] init];
+  [gp setValue: @"GNUstep" forProperty: kCKGroupNameProperty];
+  [store addRecord: gp];
+  [gp addItem: bk];
+  [gp addItem: bk1];
+
+  [store save];
+  [[NSFileManager defaultManager] removeFileAtPath: path handler: nil];
+}
+#endif
 @end
