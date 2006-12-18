b39d515cea14bc0fd3a42d04236d3f91ce2d3c23
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2006-12-18 00:18:23 +0000

    Read image file from command line
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@1357 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/Private/AZBackground/AZBackground.m
+++ Services/Private/AZBackground/AZBackground.m
@@ -9,34 +9,38 @@ static AZBackground *sharedInstance;
   NSAttributedString *string;
 }
 
+- (void) setImage: (NSImage *) image;
+
 @end
 
 @implementation AZView
 
+- (void) setImage: (NSImage *) i
+{
+  ASSIGN(image, i);
+}
+
 - (id) initWithFrame: (NSRect) rect
 {
   self = [super initWithFrame: rect];
-  image = [NSImage imageNamed: @"GNUstep"];
-  string = [[NSAttributedString alloc] initWithString: @"GNUstep"];
   return self;
 }
 
 - (void) dealloc
 {
-  DESTROY(string);
+  DESTROY(image);
   [super dealloc];
 }
 
 - (void) drawRect: (NSRect) frame
 {
-  [self lockFocus];
   [[NSColor darkGrayColor] set];
   NSRectFill(frame);
 
-  [image compositeToPoint: NSMakePoint(200, 200) 
-                operation: NSCompositeSourceOver];
-  [string drawAtPoint: NSMakePoint(200, 170)];
-  [self unlockFocus];
+  if (image) {
+    [image compositeToPoint: NSZeroPoint
+                    operation: NSCompositeSourceOver];
+  } 
 }
 
 @end
@@ -54,9 +58,21 @@ static AZBackground *sharedInstance;
 - (void) applicationDidFinishLaunching:(NSNotification *) not
 {
   /* Setup drawable window */
+  NSScreen *scr = [NSScreen mainScreen];
   XWindow *win = [[XWindow alloc] initWithWindowRef: &root_win];
-  AZView *view = [[AZView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+  AZView *view = [[AZView alloc] initWithFrame: [scr frame]];
   [win setContentView: view];
+
+  NSFileManager *fm = [NSFileManager defaultManager];
+  NSProcessInfo *pi = [NSProcessInfo processInfo];
+  NSArray *args = [pi arguments];
+  if ([args count] > 1) {
+    NSString *path = [args objectAtIndex: 1];
+    if ([fm fileExistsAtPath: [path stringByStandardizingPath]]) {
+      NSImage *im = [[NSImage alloc] initWithContentsOfFile: path];
+      [view setImage: AUTORELEASE(im)];
+    }
+  }
 
   [win skipTaskbarAndPager];
   [win makeKeyAndOrderFront: self];
