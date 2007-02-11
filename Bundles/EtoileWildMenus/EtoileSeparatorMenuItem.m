deb3dbcc0d4714354084ee766c89a1b9484632ff
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-02-11 01:53:59 +0000

    Updated EtoileWildMenus to allow separator item to be identified as such
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@1413 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Bundles/EtoileWildMenus/EtoileSeparatorMenuItem.m
+++ Bundles/EtoileWildMenus/EtoileSeparatorMenuItem.m
@@ -1,5 +1,24 @@
 
+#import <Foundation/NSString.h>
 #import "EtoileSeparatorMenuItem.h"
 
+// NOTE: This implementation is based on GSMenuSeparator private class
 @implementation EtoileSeparatorMenuItem
+
+- (id) init
+{
+  self = [super initWithTitle: @"-----------"
+                       action: NULL
+                keyEquivalent: @""];
+  _enabled = NO;
+  _changesState = NO;
+
+  return self;
+}
+
+- (BOOL) isSeparatorItem
+{
+  return YES;
+}
+
 @end
