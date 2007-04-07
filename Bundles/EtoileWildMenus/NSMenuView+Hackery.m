1dfaf32a3e23c43acce2596891a6be0cd5cde66c
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-04-07 11:25:40 +0000

    Fix to make menu usable at the cost of totally breaking tear-off menu support. Now menus should close when you click anywhere outside of their menu views (until now they only closed if you clicked a menu item or somewhere in current application windows). Related to bugs #8571 #8568 and general bug report #8666
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@1656 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Bundles/EtoileWildMenus/NSMenuView+Hackery.m
+++ Bundles/EtoileWildMenus/NSMenuView+Hackery.m
@@ -33,6 +33,7 @@ enum {
   BottomBorderOffset = 1
 };
 
+
 @implementation NSMenuView (Hackery)
 - (NSRect) rectOfItemAtIndex: (int)index
 {
@@ -459,6 +460,377 @@ enum {
           [_attachedMenu nestedSetFrameOrigin: origin];
         }
     }
+}
+
+static BOOL grabbed = NO;
+
+#define MOVE_THRESHOLD_DELTA 2.0
+#define DELAY_MULTIPLIER     10
+
+/** Overriden GNUstep version to capture mouse in order catch mouse events
+    outside of menu or current application windows. We must close currently
+    opened menu (and submenus) when we grab any mouse down event out of menu 
+    views. */
+- (BOOL) trackWithEvent: (NSEvent*)event
+{
+  unsigned    eventMask = NSPeriodicMask;
+  NSDate        *theDistantFuture = [NSDate distantFuture];
+  NSPoint    lastLocation = {0,0};
+  BOOL        justAttachedNewSubmenu = NO;
+  BOOL          subMenusNeedRemoving = YES;
+  BOOL        shouldFinish = YES;
+  int        delayCount = 0;
+  int           indexOfActionToExecute = -1;
+  int        firstIndex = -1;
+  NSEvent    *original;
+  NSEventType    type;
+  NSEventType    end;
+
+  //NSLog(@"Track event in menu %@", self);
+
+  /*
+   * The original event is unused except to determine whether the method
+   * was invoked in response to a right or left mouse down.
+   * We pass the same event on when we want tracking to move into a
+   * submenu.
+   */
+  original = AUTORELEASE(RETAIN(event));
+
+  type = [event type];
+
+  if (type == NSRightMouseDown || type == NSRightMouseDragged)
+    {
+      end = NSRightMouseUp;
+      eventMask |= NSRightMouseUpMask | NSRightMouseDraggedMask;
+      eventMask |= NSRightMouseDownMask;
+    }
+  else if (type == NSOtherMouseDown || type == NSOtherMouseDragged)
+    {
+      end = NSOtherMouseUp;
+      eventMask |= NSOtherMouseUpMask | NSOtherMouseDraggedMask;
+      eventMask |= NSOtherMouseDownMask;
+    }
+  else if (type == NSLeftMouseDown || type == NSLeftMouseDragged)
+    {
+      end = NSLeftMouseUp;
+      eventMask |= NSLeftMouseUpMask | NSLeftMouseDraggedMask;
+      eventMask |= NSLeftMouseDownMask;
+    }
+  else
+    {
+      NSLog (@"Unexpected event: %d during event tracking in NSMenuView", type);
+      end = NSLeftMouseUp;
+      eventMask |= NSLeftMouseUpMask | NSLeftMouseDraggedMask;
+      eventMask |= NSLeftMouseDownMask;
+    }
+
+// The window on which we call _releaseMouse: doesn't matter, it could be 
+// different than the one we use as _captureMouse receiver.
+  if (grabbed)
+    [[self window] _releaseMouse: self];
+
+  // Next line result in XGrabPointer() call (Xlib function).
+  [[self window] _captureMouse: self];
+  grabbed = YES;
+
+  if ([self isHorizontal] == YES)
+    {
+      /*
+       * Ignore the first mouse up if nothing interesting has happened.
+       */
+      //NSLog(@"Horizontal menu view tracking");
+      shouldFinish = NO;
+    }
+  do
+    {
+      if (type == end)
+        {
+          shouldFinish = YES;
+        }
+      if (type == NSPeriodic || event == original)
+        {
+          NSPoint    location;
+          int           index;
+
+          location     = [_window mouseLocationOutsideOfEventStream];
+          index        = [self indexOfItemAtPoint: location];
+
+      if (event == original)
+        {
+          firstIndex = index;
+        }
+      if (index != firstIndex)
+        {
+          shouldFinish = YES;
+        }
+
+          /*
+           * 1 - if menus is only partly visible and the mouse is at the
+           *     edge of the screen we move the menu so it will be visible.
+           */ 
+          if ([_attachedMenu isPartlyOffScreen])
+            {
+              NSPoint pointerLoc = [_window convertBaseToScreen: location];
+              /*
+               * The +/-1 in the y - direction is because the flipping
+               * between X-coordinates and GNUstep coordinates let the
+               * GNUstep screen coordinates start with 1.
+               */
+              if (pointerLoc.x == 0 || pointerLoc.y == 1
+                || pointerLoc.x == [[_window screen] frame].size.width - 1
+                || pointerLoc.y == [[_window screen] frame].size.height)
+                [_attachedMenu shiftOnScreen];
+            }
+
+
+          /*
+           * 2 - Check if we have to reset the justAttachedNewSubmenu
+           * flag to NO.
+           */
+          if (justAttachedNewSubmenu && index != -1
+            && index != _highlightedItemIndex)
+            { 
+              if (location.x - lastLocation.x > MOVE_THRESHOLD_DELTA)
+                {
+                  delayCount ++;
+                  if (delayCount >= DELAY_MULTIPLIER)
+                    {
+                      justAttachedNewSubmenu = NO;
+                    }
+                }
+              else
+                {
+                  justAttachedNewSubmenu = NO;
+                }
+            }
+
+
+          // 3 - If we have moved outside this menu, take appropriate action
+          if (index == -1)
+            {
+              NSPoint   locationInScreenCoordinates;
+              NSWindow *windowUnderMouse;
+              NSMenu   *candidateMenu;
+
+              subMenusNeedRemoving = NO;
+
+              locationInScreenCoordinates
+                = [_window convertBaseToScreen: location];
+
+              /*
+               * 3a - Check if moved into one of the ancester menus.
+               *      This is tricky, there are a few possibilities:
+               *          We are a transient attached menu of a
+               *          non-transient menu
+               *          We are a non-transient attached menu
+               *          We are a root: isTornOff of AppMenu
+               */
+              candidateMenu = [_attachedMenu supermenu];
+              while (candidateMenu  
+                && !NSMouseInRect (locationInScreenCoordinates, 
+                [[candidateMenu window] frame], NO) // not found yet
+                && (! ([candidateMenu isTornOff] 
+                && ![candidateMenu isTransient]))  // no root of display tree
+                && [candidateMenu isAttached]) // has displayed parent
+                {
+                  candidateMenu = [candidateMenu supermenu];
+                }
+
+              if (candidateMenu != nil
+                && NSMouseInRect (locationInScreenCoordinates,
+                [[candidateMenu window] frame], NO))
+                {
+                  BOOL candidateMenuResult;
+
+                  // The call to fetch attachedMenu is not needed. But putting
+                  // it here avoids flicker when we go back to an ancestor 
+                  // menu and the attached menu is already correct.
+                  [[[candidateMenu attachedMenu] menuRepresentation]
+                    detachSubmenu];
+
+                  // Reset highlighted index for this menu.
+                  // This way if we return to this submenu later there 
+                  // won't be a highlighted item.
+                  [[[candidateMenu attachedMenu] menuRepresentation]
+                    setHighlightedItemIndex: -1];
+
+                  candidateMenuResult = [[candidateMenu menuRepresentation]
+                           trackWithEvent: original];
+
+                  return candidateMenuResult;
+                }
+
+              // 3b - Check if we enter the attached submenu
+              windowUnderMouse = [[_attachedMenu attachedMenu] window];
+              if (windowUnderMouse != nil
+                && NSMouseInRect (locationInScreenCoordinates,
+                [windowUnderMouse frame], NO))
+                {
+                  BOOL wasTransient = [_attachedMenu isTransient];
+                  BOOL subMenuResult;
+
+                  subMenuResult
+                    = [[self attachedMenuView] trackWithEvent: original];
+                  if (subMenuResult
+                    && wasTransient == [_attachedMenu isTransient])
+                    {
+                      [self detachSubmenu];
+                    }
+                  return subMenuResult;
+                }
+            }
+
+          // 4 - We changed the selected item and should update.
+          if (!justAttachedNewSubmenu && index != _highlightedItemIndex)
+            {
+              subMenusNeedRemoving = NO;
+              [self detachSubmenu];
+              [self setHighlightedItemIndex: index];
+
+              // WO: Question?  Why the ivar _items_link
+              if (index >= 0 && [[_items_link objectAtIndex: index] submenu])
+                {
+                  [self attachSubmenuForItemAtIndex: index];
+                  justAttachedNewSubmenu = YES;
+                  delayCount = 0;
+                }
+            }
+
+          // Update last seen location for the justAttachedNewSubmenu logic.
+          lastLocation = location;
+        }
+
+  //if ([event type] != NSPeriodic)
+    //NSLog(@"In menu view pick %@", event);
+
+      event = [NSApp nextEventMatchingMask: eventMask
+        untilDate: theDistantFuture
+        inMode: NSEventTrackingRunLoopMode
+        dequeue: YES];
+      type = [event type];
+    }
+  while (type != end || shouldFinish == NO);
+
+  /*
+   * Ok, we released the mouse
+   * There are now a few possibilities:
+   * A - We released the mouse outside the menu.
+   *     Then we want the situation as it was before
+   *     we entered everything.
+   * B - We released the mouse on a submenu item
+   *     (i) - this was highlighted before we started clicking:
+   *           Remove attached menus
+   *     (ii) - this was not highlighted before pressed the mouse button;
+   *            Keep attached menus.
+   * C - We released the mouse above an ordinary action:
+   *     Execute the action.
+   *
+   *  In case A, B and C we want the transient menus to be removed
+   *  In case A and C we want to remove the menus that were created
+   *  during the dragging.
+   *
+   *  So we should do the following things:
+   * 
+   * 1 - Stop periodic events,
+   * 2 - Determine the action.
+   * 3 - Remove the Transient menus from the screen.
+   * 4 - Perform the action if there is one.
+   */
+
+  [NSEvent stopPeriodicEvents];
+
+  if (grabbed)
+    [[self window] _releaseMouse: self];
+
+  //NSLog(@"Finished menu tracking %@", self);
+
+  /*
+   * We need to store this, because _highlightedItemIndex
+   * will not be valid after we removed this menu from the screen.
+   */
+  indexOfActionToExecute = _highlightedItemIndex;
+
+  // remove transient menus. --------------------------------------------
+    {
+      NSMenu *currentMenu = _attachedMenu;
+
+      while (currentMenu && ![currentMenu isTransient])
+        {
+          currentMenu = [currentMenu attachedMenu];
+        }
+
+      while ([currentMenu isTransient] && [currentMenu supermenu])
+        {
+          currentMenu = [currentMenu supermenu];
+        }
+
+      [[currentMenu menuRepresentation] detachSubmenu];
+
+      if ([currentMenu isTransient])
+        {
+          [currentMenu closeTransient];
+        }
+    }
+
+  // ---------------------------------------------------------------------
+  if (indexOfActionToExecute == -1)
+    {
+      return YES;
+    }
+
+  if (indexOfActionToExecute >= 0
+    && [_attachedMenu attachedMenu] != nil && [_attachedMenu attachedMenu] ==
+    [[_items_link objectAtIndex: indexOfActionToExecute] submenu])
+    {
+#if 1
+      //if (NSInterfaceStyleForKey(@"NSMenuInterfaceStyle", self)
+    //== NSMacintoshInterfaceStyle)
+    {
+      /*
+       * FIXME ... always remove submenus in mac mode ... this is not
+       * quite the way the mac behaves, but it's closer than the normal
+       * behavior.
+       */
+          subMenusNeedRemoving = YES;
+    }
+#endif
+      if (subMenusNeedRemoving)
+        {
+          [self detachSubmenu];
+        }
+      // Clicked on a submenu.
+      return NO;
+    }
+
+  //if (NSInterfaceStyleForKey(@"NSMenuInterfaceStyle", self)
+  //  == NSMacintoshInterfaceStyle)
+    {
+      NSMenu    *tmp = _attachedMenu;
+
+      do
+    {
+      if ([tmp isEqual: [NSApp mainMenu]] == NO)
+        {
+          [tmp close];
+        }
+      tmp = [tmp supermenu];
+    }
+      while (tmp != nil);
+    }
+
+  [_attachedMenu performActionForItemAtIndex: indexOfActionToExecute];
+
+  /*
+   * Remove highlighting.
+   * We first check if it still highlighted because it could be the
+   * case that we choose an action in a transient window which
+   * has already dissappeared.  
+   */
+  if (_highlightedItemIndex >= 0)
+    {
+      [self setHighlightedItemIndex: -1];
+    }
+  return YES;
 }
 
 @end
