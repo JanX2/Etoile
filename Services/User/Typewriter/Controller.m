618917be4505c83b3d19a4dfb10322d0973bc226
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-04-13 05:02:51 +0000

    Add 'Open selected file' service
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@1683 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/User/Typewriter/Controller.m
+++ Services/User/Typewriter/Controller.m
@@ -9,17 +9,34 @@
 }
 
 /* Services */
-- (void) newDocumentWithSelection: (NSPasteboard *) pboard
-                         userData: (NSString *) userData
-                            error: (NSString **) error
+- (void) openDocumentWithPath: (NSPasteboard *) pboard
+                     userData: (NSString *) userData
+                        error: (NSString **) error
 {
-  NSDocumentController *docController = [NSDocumentController sharedDocumentController];
   if ([[pboard types] containsObject: NSStringPboardType])
   {
     /* Should be string */
     NSString *string = [pboard stringForType: NSStringPboardType];
     if (string)
     {
+      NSDocumentController *docController = [NSDocumentController sharedDocumentController];
+      [docController openDocumentWithContentsOfFile: [string stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]]
+                                            display: YES];
+    }
+  }
+}
+
+- (void) newDocumentWithSelection: (NSPasteboard *) pboard
+                         userData: (NSString *) userData
+                            error: (NSString **) error
+{
+  if ([[pboard types] containsObject: NSStringPboardType])
+  {
+    /* Should be string */
+    NSString *string = [pboard stringForType: NSStringPboardType];
+    if (string)
+    {
+      NSDocumentController *docController = [NSDocumentController sharedDocumentController];
       TWDocument *doc = (TWDocument *)[docController openUntitledDocumentOfType: @"TWRTFTextType" display: YES];
       [doc appendString: string];
     }
