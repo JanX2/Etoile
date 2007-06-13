c7eb7b4eff22692b557a0fb93d64ea6860b0024a
jross <jross@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-06-13 21:51:55 +0000

    Fixed issue causing it to not compile on OS X.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2038 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/User/Jabber/RosterController.m
+++ Services/User/Jabber/RosterController.m
@@ -162,7 +162,7 @@ NSMutableArray * rosterControllers = nil;
 {
 	NSAttributedString * attributedText= [self displayStringForObject:anObject];
 	float myWidth = [attributedText size].width;
-	for(unsigned int i=0 ; i<[self outlineView:view numberOfChildrenOfItem:anObject] ; i++)
+	for(int i=0 ; i<[self outlineView:view numberOfChildrenOfItem:anObject] ; i++)
 	{
 		attributedText = [self displayStringForObject:[self outlineView:view child:i ofItem:anObject]];
 		float width = [attributedText size].width + anIndent;
