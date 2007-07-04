7edf8aa04fe4f5c82f08586711418f06e8525df3
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-07-04 12:02:25 +0000

    Added some additional cleaning of GNOME/Linux stuff in user directory
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2127 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LiveCD/Subscripts/live-cleanup.sh
+++ LiveCD/Subscripts/live-cleanup.sh
@@ -11,6 +11,23 @@ umount /proc
 umount /sys # Looks not really necessary...
 umount /dev && rm /dev/null
 
+# Linux/GNOME specific cleanup
+rm -f /home/$ETOILE_USER_NAME/.bash_history
+rm -f /home/$ETOILE_USER_NAME/.esd_auth
+rm -f /home/$ETOILE_USER_NAME/.gtkrc-1.2-gnome2
+rm -f /home/$ETOILE_USER_NAME/.lesshst
+rm -f /home/$ETOILE_USER_NAME/.recently-used.xbel
+rm -f /home/$ETOILE_USER_NAME/.sudo_as_admin_successful
+rm -f /home/$ETOILE_USER_NAME/.xsession-errors
+rm -rf /home/$ETOILE_USER_NAME/.gnome
+rm -rf /home/$ETOILE_USER_NAME/.gnome2
+rm -rf /home/$ETOILE_USER_NAME/.gnome2_private
+rm -rf /home/$ETOILE_USER_NAME/.gstreamer-0.10
+rm -rf /home/$ETOILE_USER_NAME/.metacity
+rm -rf /home/$ETOILE_USER_NAME/.nautilus
+rm -rf /home/$ETOILE_USER_NAME/.Trash
+rm -rf /home/$ETOILE_USER_NAME/.update-notifier
+
 # Etoile specific cleanup
 rm -f /root/GNUstep/Defaults/*
 cp /home/$ETOILE_USER_NAME/GNUstep/Defaults/.GNUstepDefaults.original /home/$ETOILE_USER_NAME/GNUstep/Defaults/.GNUstepDefaults
