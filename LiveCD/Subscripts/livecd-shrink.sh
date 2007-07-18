0117a4a6899339808fce566f6d5a6471cabf77e1
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-07-18 11:37:52 +0000

    Updated livecd-shrink.sh to fix locale related warnings and added some notes
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2225 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LiveCD/Subscripts/livecd-shrink.sh
+++ LiveCD/Subscripts/livecd-shrink.sh
@@ -36,6 +36,9 @@
 #^libbrlapi* ^libatk1*
 #^gtk2* ^libgtk*
 
+# NOTE: Removing gnome-desktop-environment metapackage should be pretty good,
+# I suppose it removes openoffice.org metapackage by the way.
+
 apt-get remove alacarte apport-gtk at-spi ^brltty* bug-buddy capplets-data cli-common contact-lookup-applet deskbar-applet desktop-effects diveintopython docbook-xml ekiga eog evince ^evolution* f-spot file-roller ^gaim* gcalctool gcc-3.3-base ^gedit* gimp gparted gstreamer0.10-gnomevfs ^gtkhtml* ^guile* hwdb-client-gnome ^language-pack-* libapr1 ^libatspi* ^libaudio* ^libaudiofile* ^libbeagle* ^libbonobo* ^libcamel* ^libdjvulibre* ^libebook* ^libecal* ^libedata* ^libeel* ^libegroupwise* ^libexchange-storage* ^libgadu* ^libgail* ^libgamin* ^libgimp*  libglib-perl libglib2.0-cil libgmime2.2-cil  ^libgucharmap* ^libguile* libhsqldb-java ^libhtml* ^libjaxp* libjline-java ^liblaunchpad-integration* ^liblircclient* ^liblpint-bonobo* ^libmetacity* ^libmono* ^libnautilus* libnet-dbus-perl ^libnm-glib* ^libnspr* ^libopal*  ^libparted* ^libperl*  libpoppler1-glib ^libqthreads*  ^libscrollkeeper* ^libsdl* libservlet2.3-java ^libsexy* ^libsigc++* ^libsoup* ^libtotem-plparser* ^libuniconf* liburi-perl libvisual* ^libvte* ^libwmf* ^libwnck* ^libwpd8c2a ^libwps* ^libwvstreams* libwww-perl libxalan2-java  libxerces2-java libxml-parser-perl libxml-twig-perl ^libxplc* ^metacity* ^mono-* mscompress ^nautilus* ^network-manager* ^openoffice.org* rdesktop rhythmbox scim-gtk2-immodule scrollkeeper serpentine software-properties-gtk sound-juicer ssh-askpass-gnome thunderbird-locale-en-gb tomboy totem tsclient ^update-manager* update-notifier vino w3m yelp
 
 deborphan | xargs apt-get remove --purge -y # deborphan --guess-all
@@ -43,8 +46,8 @@ dpkg --list | grep ^rc | awk '{print $2}'
 ... et pour les purger :
 # COLUMNS=300 dpkg --list | grep ^rc | awk '{print $2}' | xargs dpkg -P
 
-# To be sure some critical packages haven't been removed during the last step
-apt-get -y install locales ssh firefox #language-pack-en-base
+# To be sure no critical packages have been removed
+apt-get -y install language-pack-en ssh firefox
 
 # On firefox install at this point, apt-get reports:
 #The following extra packages will be installed:
@@ -62,7 +65,16 @@ apt-get -y install locales ssh firefox #language-pack-en-base
 #The following NEW packages will be installed:
 #  openssh-server ssh
 
-apt-get check
+# NOTE: To avoid any warnings of this style after the removing of some packages
+# reinstall language-pack-en
+#XXX: warning: Setting locale failed.
+#XXX: warning: Please check that your locale settings:
+#       LANGUAGE = (unset),
+#       LC_ALL = (unset),
+#       LANG = "en_US.UTF-8"
+#   are supported and installed on your system.
+#XXX: warning: Falling back to the standard locale ("C").
 
-#ubiquity
+# A last check to verify package tree integrity
+apt-get check
 
