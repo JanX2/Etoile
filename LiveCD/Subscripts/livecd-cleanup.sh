fb1f62dc5cb3c400f6ac24a0efaa7d84d08026c1
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-07-18 10:22:30 +0000

    Looks like I never really committed livecd-cleanup.sh script until now
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2222 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ LiveCD/Subscripts/livecd-cleanup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+#
+# --cleanup
+#
+
+apt-get clean
+
+# Linux/GNOME specific cleanup
+rm -f /home/$ETOILE_USER_NAME/.bash_history
+rm -f /home/$ETOILE_USER_NAME/.esd_auth
+rm -f /home/$ETOILE_USER_NAME/.gtkrc-1.2-gnome2
+rm -f /home/$ETOILE_USER_NAME/.lesshst
+rm -f /home/$ETOILE_USER_NAME/.recently-used.xbel
+rm -f /home/$ETOILE_USER_NAME/.sudo_as_admin_successful
+rm -f /home/$ETOILE_USER_NAME/.xsession-errors
+rm -rf /home/$ETOILE_USER_NAME/.gnome
+rm -rf /home/$ETOILE_USER_NAME/.gnome2
+rm -rf /home/$ETOILE_USER_NAME/.gnome2_private
+rm -rf /home/$ETOILE_USER_NAME/.gstreamer-0.10
+rm -rf /home/$ETOILE_USER_NAME/.metacity
+rm -rf /home/$ETOILE_USER_NAME/.nautilus
+rm -rf /home/$ETOILE_USER_NAME/.Trash
+rm -rf /home/$ETOILE_USER_NAME/.update-notifier
+
+# Etoile specific cleanup
+rm -f /root/GNUstep/Defaults/*
+cp /home/$ETOILE_USER_NAME/GNUstep/Defaults/.GNUstepDefaults.original /home/$ETOILE_USER_NAME/GNUstep/Defaults/.GNUstepDefaults
+# Don't clean up AZDock because it contains Firefox cached icon actually
+#rm -r /home/$ETOILE_USER_NAME/GNUstep/Library/ApplicationSupport/AZDock
+rm -r /home/$ETOILE_USER_NAME/GNUstep/Library/Addresses
+rm -r /home/$ETOILE_USER_NAME/GNUstep/Library/Bookmark
+
+# Build cleanup
+
+rm -r /build
+apt-get -y uninstall subversion
+
+exit
+
