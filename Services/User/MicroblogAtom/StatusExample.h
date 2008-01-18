9a7a48437dca6fde12a461fcac9196d26691bb72
theraven <theraven@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2008-01-18 15:57:22 +0000

    Started working on atom microblog thing.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2784 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Services/User/MicroblogAtom/StatusExample.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  distn
+//
+//  Created by David Chisnall on 20/05/2007.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StatusExample : NSObject {
+	NSString * lastStatus;
+}
+
+@end
---
+++ Services/User/MicroblogAtom/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  distn
+//
+//  Created by David Chisnall on 20/05/2007.
+//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
