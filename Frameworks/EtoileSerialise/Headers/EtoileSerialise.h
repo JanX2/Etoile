9e7a96f2223b1805979c7e820e123bd2ee224b8f
theraven <theraven@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2008-01-20 17:41:19 +0000

    Added missing header to svn.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@2795 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Frameworks/EtoileSerialise/Headers/EtoileSerialise.h
@@ -0,0 +1,7 @@
+#import <EtoileSerialise/COProxy.h>
+#import <EtoileSerialise/ETDeserialiser.h>
+#import <EtoileSerialise/ETDeserialiserBinaryFile.h>
+#import <EtoileSerialise/ETSerialiser.h>
+#import <EtoileSerialise/ETSerialiserBackendBinaryFile.h>
+#import <EtoileSerialise/ETSerialiserBackendExample.h>
+#import <EtoileSerialise/ETSerialiserNullBackend.h>
