20a7090624a7f25f32ed75e77e3e55de123e5607
ericwa <ericwa@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2008-08-15 23:20:36 +0000

    Modified Melodie to use SmalltalkCompiler
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@3512 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/User/Melodie/main.m
+++ Services/User/Melodie/main.m
@@ -2,38 +2,11 @@
 #import <AppKit/AppKit.h>
 #import <SmalltalkKit/SmalltalkKit.h>
 
-extern int DEBUG_DUMP_MODULES;
-
-static BOOL compileString(NSString *s)
-{
-	Parser * p = [[Parser alloc] init];
-	AST *ast;
-	NS_DURING
-		ast = [p parseString: s];
-	NS_HANDLER
-		NSDictionary *e = [localException userInfo];
-		NSLog(@"Parse error in %@ on line %@.  Unexpected token at character %@ while parsing:\n%@",
-		                                                                   s,
-										   [e objectForKey:@"lineNumber"],
-										   [e objectForKey:@"character"],
-										   [e objectForKey:@"line"]);
-		NS_VALUERETURN(NO, BOOL);
-	NS_ENDHANDLER	
-	id cg = defaultCodeGenerator();
-	DEBUG_DUMP_MODULES = 0;
-	[ast compileWith:cg];
-	return YES;
-}
-
 int main(int argc, char **argv)
 {
-	[NSAutoreleasePool new];
-	NSString *Program = [NSString stringWithContentsOfFile:@"Melodie.st"];
-	if (!compileString(Program))
-	{
-		NSLog(@"Failed to compile Melodie.st");
-		return 2;
-	}
+	id pool = [NSAutoreleasePool new];
+	[SmalltalkCompiler compileString: [NSString stringWithContentsOfFile:@"Melodie.st"]];
+	[pool release];
 
 	return NSApplicationMain(argc, (const char **) argv);
 }
