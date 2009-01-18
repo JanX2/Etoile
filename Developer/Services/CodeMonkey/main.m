a7619b67c8c666fef9b22fb1065c9d7ba40eaab5
rio <rio@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2009-01-18 18:46:28 +0000

    * Moves the NSFont -italic category from main.m to PrettyPrint.st
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@4271 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Services/CodeMonkey/main.m
+++ Developer/Services/CodeMonkey/main.m
@@ -2,18 +2,6 @@
 #import <SmalltalkKit/SmalltalkKit.h>
 #import <LanguageKit/LKMessageSend.h>
 
-@interface NSFont (italic)
-- (NSFont*) italic;
-@end
-
-@implementation NSFont (italic)
-- (NSFont*) italic
-{
-	NSFontManager* fm = [NSFontManager sharedFontManager];
-	return [fm convertFont: self toHaveTrait: NSItalicFontMask];
-}
-@end
-
 int main (int argc, char** argv)
 {
 	NSAutoreleasePool* pool = [NSAutoreleasePool new];
