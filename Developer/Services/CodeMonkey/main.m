c322ec0952f92923c14871382e1119fae2967cbd
rio <rio@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2009-01-18 18:16:04 +0000

    * Moves the LKMessageSend -prettyprintToWriter to PrettyPrint.st
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@4269 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Services/CodeMonkey/main.m
+++ Developer/Services/CodeMonkey/main.m
@@ -14,50 +14,6 @@
 }
 @end
 
-@protocol writer
-- (void) red: (NSString*) str;
-- (void) append: (NSString*) str;
-@end
-
-@interface LKMessageSend (writer)
-- (void) prettyprintToWriter: (id<writer>) writer;
-@end
-
-@implementation LKMessageSend (writer)
-- (void) prettyprintToWriter: (id<writer>) writer
-{
-	[target prettyprintToWriter: writer];
-	[writer append: @" "];
-	NSArray* sel = [[selector description] componentsSeparatedByString: @":"];
-	if ([sel count] == 1)	
-	{
-		[writer red: [selector description]];
-	}
-	else
-	{
-		[writer red: [sel objectAtIndex: 0]];
-	}
-	NSLog (@"<%@>(%d)", selector, [arguments count]);
-	if ([arguments count])
-	{
-		[writer red: @": "];
-		[[arguments objectAtIndex: 0] prettyprintToWriter: writer];
-		//[writer append: [[arguments objectAtIndex: 0] description]];
-	}
-	for (int i=1; i<[arguments count]; i++)
-	{
-		if (i < [sel count])
-		{
-			[writer append: @" "];
-			[writer red: [sel objectAtIndex: i]];
-		}
-		[writer red: @": "];
-		//[writer append: [[arguments objectAtIndex: i] description]];
-		[[arguments objectAtIndex: i] prettyprintToWriter: writer];
-	}	 
-}
-@end
-
 int main (int argc, char** argv)
 {
 	NSAutoreleasePool* pool = [NSAutoreleasePool new];
