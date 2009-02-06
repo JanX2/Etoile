0cfa10b3648d572355278d750504e3a13c5a5c24
ericwa <ericwa@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2009-02-06 02:13:53 +0000

    Move Installer to /branches (experimental, and GPL licensed)
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@4412 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/User/Installer/ProgressController.h
+++
@@ -1,8 +0,0 @@
-/* All Rights reserved */
-
-#include <AppKit/AppKit.h>
-
-@interface ProgressController : NSObject
-{
-}
-@end
--- Services/User/Installer/ProgressController.m
+++
@@ -1,8 +0,0 @@
-/* All Rights reserved */
-
-#include <AppKit/AppKit.h>
-#include "ProgressController.h"
-
-@implementation ProgressController
-
-@end
