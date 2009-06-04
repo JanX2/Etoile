1779abb41c855ad0ace9ef9b50c36a31f44d38ec
ericwa <ericwa@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2009-06-04 23:24:40 +0000

    Update the ubuntu-install-etoile.sh script. Should produce a working etoile-stable install on Ubuntu 9.04 now
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@4750 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- LiveCD/Subscripts/ubuntu-install-etoile.sh
+++ LiveCD/Subscripts/ubuntu-install-etoile.sh
@@ -9,74 +9,82 @@ else
 fi
 cd build
 
-# Install Etoile and GNUstep dependencies
+# Install Etoile and GNUstep dependencies for Ubuntu 9.04 (copied from INSTALL.Ubuntu)
+# Universe repository needs to be enabled in /etc/apt/sources.list for libonig-dev to show up
 
-sudo apt-get -y install gobjc-4.1 openssl libssl-dev libxml2-dev libxslt1-dev libffi4-dev libjpeg62-dev libtiff4-dev libpng12-dev libungif4-dev libfreetype6-dev libx11-dev libart-2.0-dev libxft-dev libxmu-dev libxss-dev xscreensaver libdbus-1-dev libstartup-notification0-dev g++ libpoppler-dev
+sudo aptitude install gobjc-4.3 libxml2-dev libxslt1-dev libffi-dev libssl-dev libjpeg62-dev libtiff4-dev libpng12-dev libgif-dev libfreetype6-dev libx11-dev libcairo2-dev libxft-dev libxmu-dev dbus libdbus-1-dev hal libstartup-notification0-dev libxcursor-dev libxss-dev xscreensaver g++ libpoppler-dev libonig-dev  lemon libgmp3-dev postgresql libpq-dev libavcodec-dev libavformat-dev libtagc0-dev libmp4v2-dev
 
-# Install Subversion to be able to check both GNUstep and Etoile stable versions
+# Install Subversion to be able to check out Etoile
 
-sudo apt-get -y install subversion
+sudo aptitude install subversion
 
-# Check out and build GNUstep stable version
+# Check out and build the latest GNUstep release
 
-svn co http://svn.gna.org/svn/gnustep/tools/make/branches/stable gnustep-make
-svn co http://svn.gna.org/svn/gnustep/libs/base/branches/stable gnustep-base
-svn co http://svn.gna.org/svn/gnustep/libs/gui/branches/stable gnustep-gui
-svn co http://svn.gna.org/svn/gnustep/libs/back/branches/stable gnustep-back
+export GSMAKE=gnustep-make-2.2.0
+export GSBASE=gnustep-base-1.19.1
+export GSGUI=gnustep-gui-0.17.0
+export GSBACK=gnustep-back-0.17.0
+export GSGORM=gorm-1.2.10
 
-cd gnustep-make
-./configure --prefix=/ && make && sudo make install
+wget -nc ftp://ftp.gnustep.org/pub/gnustep/core/$GSMAKE.tar.gz
+tar -xzf $GSMAKE.tar.gz
+wget -nc ftp://ftp.gnustep.org/pub/gnustep/core/$GSBASE.tar.gz
+tar -xzf $GSBASE.tar.gz
+wget -nc ftp://ftp.gnustep.org/pub/gnustep/core/$GSGUI.tar.gz
+tar -xzf $GSGUI.tar.gz
+wget -nc ftp://ftp.gnustep.org/pub/gnustep/core/$GSBACK.tar.gz
+tar -xzf $GSBACK.tar.gz
+
+# Build & Install GNUstep
+
+cd $GSMAKE
+./configure --prefix=/ && make && sudo -E make install
 cd ..
+
+# Source the GNUstep shell script, and add it to the user's bashrc
 . /System/Library/Makefiles/GNUstep.sh
-cd gnustep-base
-./configure && make && sudo make install
-cd ..
-cd gnustep-gui
-./configure && make && sudo make install
-cd ..
-cd gnustep-back
-./configure && make && sudo make install
-cd ..
+echo ". /System/Library/Makefiles/GNUstep.sh" >> ~/.bashrc
 
-# Download latest StepTalk release over FTP, uncompress and build it
-
-wget ftp://ftp.gnustep.org/pub/gnustep/libs/StepTalk-0.10.0.tar.gz
-tar -xzf StepTalk-0.10.0.tar.gz
-cd StepTalk
-make && sudo make install
+cd $GSBASE
+./configure && make && sudo -E make install
+cd ..
+cd $GSGUI
+./configure && make && sudo -E make install
+cd ..
+cd $GSBACK
+./configure --enable-graphics=cairo && make && sudo -E make install
 cd ..
 
 # Download latest Gorm release over FTP, uncompress and build it
 
-wget ftp://ftp.gnustep.org/pub/gnustep/dev-apps/gorm-1.2.1.tar.gz
-tar -xzf gorm-1.2.1.tar.gz
+wget -nc ftp://ftp.gnustep.org/pub/gnustep/dev-apps/$GSGORM.tar.gz
+tar -xzf $GSGORM.tar.gz
 
-cd gorm-1.2.1
-make && sudo make install
+cd $GSGORM
+make && sudo -E make install
+cd ..
+
+# Download and install LLVM 2.5
+
+# Also apply a patch for bug http://llvm.org/bugs/show_bug.cgi?id=3801
+# , otherwise all Smalltalk tests crash on Linux/x86
+# TODO: remove this once the bug is fixed.
+
+wget -nc http://llvm.org/releases/2.5/llvm-2.5.tar.gz
+tar -xzf llvm-2.5.tar.gz
+cd llvm-2.5
+wget -O llvm.patch http://llvm.org/bugs/attachment.cgi?id=2744
+sudo aptitude install patch
+patch -p0 < llvm.patch
+./configure && make && sudo make install
 cd ..
 
 # Check out and build Etoile stable version
 
-svn co http://svn.gna.org/svn/etoile/trunk/Dependencies Dependencies
 svn co http://svn.gna.org/svn/etoile/stable/Etoile Etoile
 
-cd Dependencies/oniguruma5
-./configure && make && sudo make install # Build and install Oniguruma first
-cd ../..
 cd Etoile
-make && sudo make install
-
-# Register /usr/local/lib so libonig can be found by OgreKit
-if [ -z `sed -n -e '/\/usr\/local\/lib/p' /etc/ld.so.conf` ]; then
-	sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf'
-fi
-sudo ldconfig
-
-# Workaround install bug (probably related to gnustep-make)
-# FIXME: Remove this hack.
-
-sudo ln -s /Library/StepTalk /Local/Library/StepTalk
-#ln -s /Library/Grr /Local/Library/Grr
+make && sudo -E make install
 
 echo
 echo "Installation of Etoile is almost finished, you now need to run setup.sh "
