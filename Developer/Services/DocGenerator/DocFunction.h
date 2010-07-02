608651e9d3dfe4f67c4d6bf5bb53fd9f987c4ba9
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2010-07-02 11:29:37 +0000

    Reformatted DocFunction. Updated to use FOREACH and SUPERINIT too.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@6200 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Services/DocGenerator/DocFunction.h
+++ Developer/Services/DocGenerator/DocFunction.h
@@ -15,19 +15,19 @@
 
 @interface DocFunction : DocElement <GSDocParserDelegate>
 {
-  NSMutableArray* parameters; 
-  NSString* returnType;
-  NSString* returnDescription; 
-  NSString* task;
+	NSMutableArray *parameters; 
+	NSString *returnType;
+	NSString *returnDescription; 
+	NSString *task;
 }
 
-- (NSString*) task;
-- (void) setTask: (NSString*) aTask;
-- (void) setReturnType: (NSString*) aReturnType;
-- (void) addParameter: (NSString*) aName ofType: (NSString*) aType;
-- (NSString*) content;
-- (void) setDescription: (NSString*) aDescription forParameter: (NSString*) aName;
-- (void) setReturnDescription: (NSString*) aDescription;
-- (HtmlElement*) richDescription;
+- (NSString *)task;
+- (void) setTask: (NSString *)aTask;
+- (void) setReturnType: (NSString *)aReturnType;
+- (void) addParameter: (NSString *)aName ofType: (NSString* )aType;
+- (NSString *) content;
+- (void) setDescription: (NSString *)aDescription forParameter: (NSString *)aName;
+- (void) setReturnDescription: (NSString *)aDescription;
+- (HtmlElement *) richDescription;
 
 @end
--- Developer/Services/DocGenerator/DocFunction.m
+++ Developer/Services/DocGenerator/DocFunction.m
@@ -15,152 +15,149 @@
 
 - (id) init
 {
-  self = [super init];
-  parameters = [NSMutableArray new];
-  task = [[NSString alloc] initWithString: @"Default"];
-  return self;
+	SUPERINIT;
+	parameters = [NSMutableArray new];
+	task = [[NSString alloc] initWithString: @"Default"];
+	return self;
 }
 
 - (void) dealloc
 {
-  [parameters release];
-  [task release];
-  [returnDescription release];
-  [task release];
-  [returnType release];
-  [super dealloc];
+	[parameters release];
+	[task release];
+	[returnDescription release];
+	[task release];
+	[returnType release];
+	[super dealloc];
 }
 
 - (NSString *) description
 {
 	return [NSString stringWithFormat: @"%@ - %@, %@", [super description], 
-		name, [self task]];
+			name, [self task]];
 }
 
-- (NSComparisonResult) caseInsensitiveCompare: (NSString *) aString
+- (NSComparisonResult) caseInsensitiveCompare: (NSString *)aString
 {
-  return [aString caseInsensitiveCompare: name];
+	return [aString caseInsensitiveCompare: name];
 }
 
-- (NSString*) task
+- (NSString *) task
 {
-  return task;
+	return task;
 }
 
-- (void) setTask: (NSString*) aTask
+- (void) setTask: (NSString *)aTask
 {
-  [aTask retain];
-  [task release];
-  task = aTask;
+	ASSIGN(task, aTask);
 }
 
-- (void) setReturnType: (NSString*) aReturnType
+- (void) setReturnType: (NSString *) aReturnType
 {
-  [aReturnType retain];
-  [returnType release];
-  returnType = aReturnType;
+	ASSIGN(returnType, aReturnType);
 }
 
-- (void) setReturnDescription: (NSString*) aDescription
+- (void) setReturnDescription: (NSString *)aDescription
 {
-  [aDescription retain];
-  [returnDescription release];
-  returnDescription = aDescription;
+	ASSIGN(returnDescription, aDescription);
 }
 
-- (void) addParameter: (NSString*) aName ofType: (NSString*) aType
+- (void) addParameter: (NSString *)aName ofType: (NSString *)aType
 {
-//  [parameters addObject: [NSDictionary dictionaryWithObjectsAndKeys: aName, @"name", aType, @"type", nil]];
-  [parameters addObject: [Parameter newWithName: aName andType: aType]];
+	//  [parameters addObject: [NSDictionary dictionaryWithObjectsAndKeys: aName, @"name", aType, @"type", nil]];
+	[parameters addObject: [Parameter newWithName: aName andType: aType]];
 }
 
-- (void) setDescription: (NSString*) aDescription forParameter: (NSString*) aName
+- (void) setDescription: (NSString *)aDescription forParameter: (NSString *)aName
 {
-  for (int i=0; i<[parameters count]; i++)
-  {
-    Parameter* p = [parameters objectAtIndex: i];
-    if ([[p name] isEqualToString: aName])
-    {
-      [p setDescription: aDescription];
-      return;
-    }
-  }
+	FOREACH(parameters, p, Parameter *)
+	{
+		if ([[p name] isEqualToString: aName])
+		{
+			[p setDescription: aDescription];
+			return;
+		}
+	}
 }
 
-- (void) addInformationFrom: (DescriptionParser*) aParser
+- (void) addInformationFrom: (DescriptionParser *)aParser
 {
-  [super addInformationFrom: aParser];
-
-  for (int i=0; i<[parameters count]; i++)
-  {
-    Parameter* p = [parameters objectAtIndex: i];
-    [p setDescription: [aParser descriptionForParameter: [p name]]];
-  }    
-  //NSLog (@"Parser return description <%@>", [aParser returnDescription]);
-  [self setReturnDescription: [aParser returnDescription]]; 
-  [self setTask: [aParser task]];
+	[super addInformationFrom: aParser];
+	
+	FOREACH(parameters, p, Parameter *)
+	{
+		[p setDescription: [aParser descriptionForParameter: [p name]]];
+	}    
+	//NSLog (@"Parser return description <%@>", [aParser returnDescription]);
+	[self setReturnDescription: [aParser returnDescription]]; 
+	[self setTask: [aParser task]];
 }
 
 - (H) richDescription
 {
-  H param_list = [DIV class: @"paramsList"];
-  H ul = UL;
-  if ([parameters count] > 0)
-    [param_list and: [H3 with: @"Parameters"]];
-  for (int i=0; i<[parameters count]; i++)
-  {
-    Parameter* p = [parameters objectAtIndex: i];
-    H h_param = [LI with: [I with: [p name]]];
-    [h_param and: [p description]];
-    [ul and: h_param];
-  }
-  [param_list and: ul];
-  
-  if ([returnDescription length])
-  {
-    [param_list and: [H3 with: @"Return"]];
-    [param_list and: returnDescription];
-  }
-  
-  [param_list and: [H3 with: @"Description"]];
-  [param_list and: filteredDescription];
-  return param_list;
+	H param_list = [DIV class: @"paramsList"];
+	H ul = UL;
+
+	if ([parameters count] > 0)
+	{
+		[param_list and: [H3 with: @"Parameters"]];
+	}
+
+	FOREACH(parameters, p, Parameter *)
+	{
+		H h_param = [LI with: [I with: [p name]]];
+		[h_param and: [p description]];
+		[ul and: h_param];
+	}
+	[param_list and: ul];
+	
+	if ([returnDescription length])
+	{
+		[param_list and: [H3 with: @"Return"]];
+		[param_list and: returnDescription];
+	}
+	
+	[param_list and: [H3 with: @"Description"]];
+	[param_list and: filteredDescription];
+
+	return param_list;
 }
 
-- (NSString*) content
+- (NSString *) content
 {
-  H h_signature = [DIV class: @"methodSignature"];
-  
-  H h_returnType = [DIV class: @"returnType" with: [DIV class: @"type" with: returnType]];
-  
-  [h_signature and: h_returnType];
-  
-  [h_signature and: [DIV class: @"selector" with: name]];
-  [h_signature with: @"("];
-  
-  for (int i=0; i<[parameters count]; i++)
-  {
-    Parameter* p = [parameters objectAtIndex: i];
-    H h_parameter = [DIV class: @"parameter" with: 
-                        [DIV class: @"type" with: [p type]] 
-                            and: @" " and: [DIV class: @"arg" with: [p name]]];
-    if (i>0)
-    {
-      [h_signature and: @","];
-    }
-    [h_signature and: h_parameter];
-  }
-  [h_signature with: @")"];
-  
-  H methodFull = [DIV class: @"method" with: 
-                  [DL with: [DT with: h_signature]
-                       and: [DD with: [DIV class: @"methodDescription" with: [self richDescription]]]]];
-  return [methodFull content];
+	H h_signature = [DIV class: @"methodSignature"];
+	H h_returnType = [DIV class: @"returnType" with: [DIV class: @"type" with: returnType]];
+	
+	[h_signature and: h_returnType];
+	
+	[h_signature and: [DIV class: @"selector" with: name]];
+	[h_signature with: @"("];
+
+	BOOL isFirst = YES;
+	FOREACH(parameters, p, Parameter *)
+	{
+		H h_parameter = [DIV class: @"parameter" with: 
+		                [DIV class: @"type" with: [p type]] 
+		                       and: @" " and: [DIV class: @"arg" with: [p name]]];
+		if (NO == isFirst)
+		{
+			[h_signature and: @","];
+		}
+		[h_signature and: h_parameter];
+
+		isFirst = NO;
+	}
+	[h_signature with: @")"];
+	
+	H methodFull = [DIV class: @"method" with: 
+	                 [DL with: [DT with: h_signature]
+	                      and: [DD with: [DIV class: @"methodDescription" with: [self richDescription]]]]];
+	return [methodFull content];
 }
 
 - (void) parser: (GSDocParser *)parser 
    startElement: (NSString *)elementName
-  withAttributes: (NSDictionary *)attributeDict
+ withAttributes: (NSDictionary *)attributeDict
 {
 	if ([elementName isEqualToString: @"function"]) /* Opening tag */
 	{
@@ -187,16 +184,16 @@
 	else if ([elementName isEqualToString: @"function"]) /* Closing tag */
 	{
 		DescriptionParser* descParser = [DescriptionParser new];
-
+		
 		[descParser parse: [self rawDescription]];
-
+		
 		//NSLog(@"Function raw description <%@>", [self rawDescription]);
-
+		
 		[self addInformationFrom: descParser];
 		[descParser release];
 		
 		[parser addFunction: self];
-
+		
 		ENDLOG2(name, [self task]);
 	}
 }
