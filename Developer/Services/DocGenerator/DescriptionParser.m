7c30f8a78c92d5bce6f48e9424140e2a1ac9f7d0
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2010-12-07 12:40:27 +0000

    Rewrote the comment parser to handle paragraph markup correctly and support @taskunit
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@6635 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Services/DocGenerator/DescriptionParser.m
+++ Developer/Services/DocGenerator/DescriptionParser.m
@@ -45,18 +45,143 @@
   return (NSMutableDictionary*) [self getContent: [NSMutableDictionary class] for: tag];
 }
 
+- (NSArray *) linesForString: (NSString *)aString
+{
+	NSUInteger length = [aString length];
+	NSRange searchRange = NSMakeRange(0, 0);
+	NSMutableArray *lines = [NSMutableArray array];
+
+	while (searchRange.location < length)
+	{
+		NSUInteger startIndex = 0;
+		NSUInteger nextLineIndex = 0;
+		NSUInteger separatorIndex = 0;
+		NSRange lineRange = NSMakeRange(0, 0);
+
+		[aString getLineStart: &startIndex end: &nextLineIndex contentsEnd: &separatorIndex forRange: searchRange];
+		lineRange.location = startIndex;
+		lineRange.length = separatorIndex - startIndex;
+
+		[lines addObject: [aString substringWithRange: lineRange]];
+		//NSLog(@"Found line: %@|", [lines lastObject]);
+
+		searchRange.location = nextLineIndex;
+	}
+
+	return lines;
+}
+
+/* Returns the description extracted from 
+
+@tag + spaces + description + </p> + spaces
+
+and where </p> is optional. */ 
+- (NSString *) descriptionFromString: (NSString *)aString
+{
+	NSRange r = [aString rangeOfCharacterFromSet: [NSCharacterSet whitespaceCharacterSet]];
+	/* Skip the first word and space e.g. @task + space */
+	NSString *desc = [aString substringFromIndex: (r.location == NSNotFound ? 0 : r.location + r.length)];
+
+	desc = [desc stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceCharacterSet]];
+	if ([desc hasSuffix: @"</p>"])
+	{
+		desc = [desc substringToIndex: [desc length] - 4];
+	}
+	return desc;
+}
+
+
+
+/* Rough Grammar
+
+<TASKUNIT> <DESC>  
+<DESC(MAIN)>
+<PARAM> <PARAMNAME> <DESC>
+<RETURN> <DESC>
+<TASK> <DESC>
+
+Each line from the grammar can be wrapped in <p> and </p> pair.
+
+If TASKUNIT or DESC(MAIN) don't end with </p>, they must end with a blank line 
+such as '\n\n'.
+
+DESC(MAIN) can contain arbitrary newlines, <tag>, </tag> and <\ tag> markup, 
+unlike other grammar elements which must contain no extra markup.
+
+TASKUNIT must be located before DESC(MAIN).
+
+PARAM, RETURN and TASK must follow DESC(MAIN).
+
+PARAM, RETURN and TASK declaration order doesn't matter. */
 - (void) parse: (NSString*) corpus
 {
-  /*
-   grammar:
-  <TASKUNIT> <DESC> <p>  
-  <DESC>
-  <PARAM> <PARAMNAME> <DESC>
-  <TASK> <DESC>
-  <RETURN> <DESC>
-  
-  */
-  
+	NSMutableString *current = [self getStringFor: @"description"];
+	NSArray *lines = [self linesForString: corpus];
+	NSCharacterSet *blankCharset = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+	BOOL isParsingDocTag = NO;
+	BOOL hasParsedMainDescription = NO;
+	int nbOfLines = [lines count];
+
+	for (int i = 0; i < nbOfLines; i++)
+	{
+		NSString *line = [lines objectAtIndex: i];
+		BOOL isLastLine =  (i == (nbOfLines - 1));
+
+		if ([line hasPrefix: @"<p>@taskunit"] || [line hasPrefix: @"@taskunit"])
+		{
+			current = [self getStringFor: @"taskunit"];
+			[current appendFormat: @"%@ ", [self descriptionFromString: line]];
+
+			isParsingDocTag = YES;
+		}
+		else if ([line hasPrefix: @"<p>@task"] || [line hasPrefix: @"@task"])
+		{
+			current = [self getStringFor: @"task"];
+			[current appendFormat: @"%@ ", [self descriptionFromString: line]];
+
+			isParsingDocTag = YES;
+		}
+		else if (isParsingDocTag == NO && hasParsedMainDescription == NO)
+		{
+			current = [self getStringFor: @"description"];
+			[current appendFormat: @"%@\n", line]; 
+
+			hasParsedMainDescription = YES;
+		}
+    		else if (isParsingDocTag)
+		{
+      			[current appendFormat: @"%@ ", line];
+		}
+		else /* Parsing Main Description */
+    		{
+			if (isLastLine)
+			{
+      				[current appendString: line];
+			}
+			else
+			{
+      				[current appendFormat: @"%@\n", line];
+			}
+    		}
+
+		if (isParsingDocTag)
+		{
+			NSString *trimmedLine = [line stringByTrimmingCharactersInSet: blankCharset];
+ 			BOOL isDocTagEnd = ([trimmedLine hasSuffix: @"</p>"] || [trimmedLine isEqualToString: @""]);
+
+			if (isDocTagEnd)
+			{
+				isParsingDocTag = NO;
+				current = nil;
+			}
+		}
+	}
+
+	NSLog(@"Parsed task unit: %@", [self taskUnit]);
+	NSLog(@"Parsed desc: %@", [self description]);
+	NSLog(@"Parsed task: %@", [self task]);
+
+#if 0
   BOOL param = NO;
   BOOL paramNameSet = NO;
   BOOL hasParsedMainDescription = NO;  
@@ -106,6 +231,7 @@
       [current appendFormat: @"%@ ", word];
     }
   }
+#endif
 }
 
 - (NSString*) description
@@ -120,7 +246,7 @@
 
 - (NSString *) taskUnit
 {
-	return [self getStringFor: @"taskUnit"];
+	return [self getStringFor: @"taskunit"];
 }
 
 - (NSString*) returnDescription
