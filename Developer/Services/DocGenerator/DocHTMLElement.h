183a51c6a585b37f146e164adca468bbdb15f954
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2011-01-20 15:41:46 +0000

    Partially documented DocHTMLElement
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@6721 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Developer/Services/DocGenerator/DocHTMLElement.h
+++ Developer/Services/DocGenerator/DocHTMLElement.h
@@ -14,14 +14,36 @@
 #undef A
 #endif
 
-/** @group HTML Support */
+/** @group HTML Support
+    @abstract HTML element class to output HTML in a concise way.
+
+DocHTMLElement is a Seaside-inspired class that provides a DSL to write compact 
+HTML generation code.
+
+DocHTMLElement comes with null-like element that doesn't emit the element markup 
+when evaluated. See +blankElement.
+
+TODO: DocHTMLElement DSL should be more clean and formalized. For example, 
+-and:, -with: and -add: are the same, but it isn't entirely clear-cut when one 
+should be preferred to the others.<br />
+For now, a single -with: should be used per message chains. Any subsequent 
+concatenation messages should use -and:. e.g. -with:and:and: is valid but 
+-with:with:and: is not.<br />
+The obscure point is what to do when we send a single concatenation message. 
+Should we recommend -with:, -and: or -add:? Should we take in account the type 
+of the concatened content such DocHTMLElement or NSString, then make a 
+distinction between -add: and -addText:.<br />
+Is DocHTMLElement DSL really needed or the right answer to what we need? */
 @interface DocHTMLElement : NSObject 
 {
-	NSString* elementName;
-	NSMutableDictionary* attributes;
-	NSMutableArray* children;
+	NSString *elementName;
+	NSMutableDictionary *attributes;
+	NSMutableArray *children;
 	NSSet *blockElementNames;
 }
+
+/** @taskunit Initialization and Factory Methods */
+
 /** Returns a new HTML element whose content is not wrapped into opening and 
 closing tags corresponding to the receiver, but limited to the child content.
 
@@ -31,14 +53,34 @@ each child content is evaluated normally.
 When the receiver has no children, -content returns an empty string. */
 + (DocHTMLElement *) blankElement;
 + (DocHTMLElement *) elementWithName: (NSString *) aName;
+
 - (DocHTMLElement *) initWithName: (NSString *) aName;
-- (DocHTMLElement *) addText: (NSString *) aText;
-- (DocHTMLElement *) add: (DocHTMLElement *) anElem;
+
+/** @taskunit HTML Attributes Support */
+
 - (DocHTMLElement *) id: (NSString *) anID;
 - (DocHTMLElement *) class: (NSString *) aClass;
 - (DocHTMLElement *) name: (NSString *) aName;
+
+/** @taskunit Tree Building Primitives */
+
+- (DocHTMLElement *) addText: (NSString *) aText;
+- (DocHTMLElement *) add: (DocHTMLElement *) anElem;
+
+/** @taskunit Tree Building Syntactic Sugar */
+
 - (DocHTMLElement *) with: (id) something;
 - (DocHTMLElement *) and: (id) something;
+
+/** @taskunit HTML Generation */
+
+/** Returns the HTML string representation of the element tree rooted in the 
+receiver.
+
+This HTML generation is done in a recursive manner by invoking -content on the 
+child elements.
+
+TODO: Document a bit better and when line breaks are inserted. */
 - (NSString *) content;
 /** Returns -content. */
 - (NSString  *) description;
@@ -70,7 +112,10 @@ When the receiver has no children, -content returns an empty string. */
 #define EM [DocHTMLElement elementWithName: @"em"]
 #define HR [DocHTMLElement elementWithName: @"hr"]
 
-/** @group HTML Support */
+/** @group HTML Support
+
+Category on DocHTMLElement to prevent compiler warning since DocHTMLElement 
+has no fixed API but support a large number of message chaining variations.  */
 @interface DocHTMLElement (CommonUseCases)
 - (DocHTMLElement *) class: (NSString *)aClass with: (id)something;
 - (DocHTMLElement *) class: (NSString *)aClass with: (id)something and: (id) something;
