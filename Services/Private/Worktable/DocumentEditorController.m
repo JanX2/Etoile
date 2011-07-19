5052f4abf3567b7f344f054fe52a28e078e76342
qmathe <qmathe@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2011-07-19 11:23:38 +0000

    Wrote the missing bits to get the editor handles persistency transparently
    Major limitation: only supports a single kind of changes which is rectangle 
    shape insertion, so it isn't that interesting ;-)
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@7118 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Services/Private/Worktable/DocumentEditorController.m
+++ Services/Private/Worktable/DocumentEditorController.m
@@ -6,8 +6,12 @@
 	License:  Modified BSD (see COPYING)
  */
 
+#import <ObjectMerging/COEditingContext.h>
+#import <EtoileUI/ETLayoutItem+CoreObject.h>
 #import "DocumentEditorController.h"
 
+@interface ETCompoundDocumentTemplate : ETItemTemplate
+@end
 
 @implementation DocumentEditorController
 
@@ -22,6 +26,11 @@
 {
 	[self setUpMenus];
 
+	COEditingContext *ctxt = [COEditingContext contextWithURL: 
+		[NSURL fileURLWithPath: [@"~/TestDocumentStore" stringByExpandingTildeInPath]]];
+
+	[COEditingContext setCurrentContext: ctxt];
+
 	ETLayoutItemFactory *itemFactory = [ETLayoutItemFactory factory];
 
 	[[itemFactory windowGroup] setController: self];
@@ -33,17 +42,115 @@
 		                     typeTags: [NSDictionary dictionary]];
 
 	// TODO: Use -compoundDocumentItem
+	[itemFactory beginRootObject];
 	mainItem = [itemFactory itemGroup];
 	[mainItem setSize: NSMakeSize(500, 400)];
-	[mainItem setLayout: [ETFreeLayout layout]];
+	[mainItem setLayout: [ETFixedLayout layout]];
+	[itemFactory endRootObject];
 										 
-	[self setTemplate: [ETItemTemplate templateWithItem: mainItem objectClass: Nil]
+	[self setTemplate: [ETCompoundDocumentTemplate templateWithItem: mainItem objectClass: Nil]
 	          forType: mainType];
 
 	/* Set the type of the documented to be created by default with 'New' in the menu */
 	[self setCurrentObjectType: mainType];
 	
-	[self newDocument: nil];
+	//[self newDocument: nil];
+	//[[NSUserDefaults standardUserDefaults] removeObjectForKey: @"kETOpenedDocumentUUIDs"];
+	[self showPreviouslyOpenedDocuments];
+
+	/*ETShape *shape = [ETShape rectangleShape];
+
+	[shape becomePersistentInContext: ctxt rootObject: mainItem];*/
+}
+
+// TODO: Move the methods below to ETDocumentController once Worktable is more mature
+
+- (void) showPreviouslyOpenedDocuments
+{
+	for (ETUUID *uuid in [self openedDocumentUUIDsFromDefaults])
+	{
+		ETLayoutItemGroup *documentItem = (id)[[COEditingContext currentContext] objectWithUUID: uuid];
+		[[[ETLayoutItemFactory factory] windowGroup] addItem: documentItem];
+	}
+}
+
+- (NSArray *) openedDocumentUUIDsFromDefaults
+{
+	NSArray *openedDocUUIDStrings = [[NSUserDefaults standardUserDefaults] arrayForKey: @"kETOpenedDocumentUUIDs"];
+	NSMutableArray *openedDocUUIDs = [NSMutableArray array];
+
+	for (NSString *UUIDString in openedDocUUIDStrings)
+	{
+		[openedDocUUIDs addObject: [ETUUID UUIDWithString: UUIDString]];
+	}
+
+	return openedDocUUIDs;
+}
+
+- (void) rememberOpenedDocumentItem: (ETLayoutItem *)anItem
+{
+	NSArray *openedDocUUIDs = [self openedDocumentUUIDsFromDefaults];
+
+	if ([openedDocUUIDs containsObject: [anItem UUID]])
+		return;
+
+	openedDocUUIDs = [openedDocUUIDs arrayByAddingObject: [anItem UUID]];
+	[[NSUserDefaults standardUserDefaults] setObject: [[openedDocUUIDs mappedCollection] stringValue]
+	                                          forKey: @"kETOpenedDocumentUUIDs"];
+}
+
+- (void) rememberClosedDocumentItem: (ETLayoutItem *)anItem
+{
+	NSArray *openedDocUUIDs = [self openedDocumentUUIDsFromDefaults];
+
+	if ([openedDocUUIDs containsObject: [anItem UUID]] == NO)
+		return;
+
+	openedDocUUIDs = [openedDocUUIDs arrayByRemovingObject: [anItem UUID]];
+	[[NSUserDefaults standardUserDefaults] setObject: openedDocUUIDs 
+	                                          forKey: @"kETOpenedDocumentUUIDs"];
+}
+
+- (void) didOpenDocumentItem: (ETLayoutItem *)anItem
+{
+	[self rememberOpenedDocumentItem: anItem];
+}
+
+- (void) didCreateDocumentItem: (ETLayoutItem *)anItem
+{
+	[self rememberOpenedDocumentItem: anItem];
+	// Hmm, not sure that's the proper place to commit
+	[[COEditingContext currentContext] commit];
+}
+
+// Won't be called on quit, -terminate: doesn't close the windows with -performClose:
+- (void) willCloseDocumentItem: (ETLayoutItem *)anItem
+{
+	[self rememberClosedDocumentItem: anItem];
 }
 
 @end
+
+@implementation ETCompoundDocumentTemplate
+
+- (BOOL) writeItem: (ETLayoutItem *)anItem 
+             toURL: (NSURL *)aURL 
+           options: (NSDictionary *)options
+{
+	ETAssert([anItem compoundDocument] != nil);
+	[[anItem editingContext] commit];
+	return YES;
+}
+
+- (ETLayoutItem *) newItemWithURL: (NSURL *)aURL options: (NSDictionary *)options
+{
+	COEditingContext *editingContext = [COEditingContext currentContext];
+	NSAssert(editingContext != nil, @"Current editing context must not be nil to create a new item");
+
+	ETLayoutItem *item = [super newItemWithURL: aURL options: options];
+	[item becomePersistentInContext: editingContext rootObject: item];
+	return item;
+}
+
+@end
+
