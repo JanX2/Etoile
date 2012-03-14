493dd712aed453bf9236b8cf76dc44c09f7c88e1
mathk <mathk@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2012-03-14 15:50:48 +0000

    Add build script
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@7578 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Languages/ParserKit/compile.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#Generate bitcode for all of the Smalltalk files
+for ST in $@ 
+do
+	echo Compiling $ST to LLVM bitcode...
+	edlc -c -f $ST -v LKLowerIfTrueTransform
+done
+
+# Link the bitcode together
+BC=`echo $@ | sed 's/\.st/\.bc/g'`
+echo Linking bitcode files...
+llvm-link -f -o smalltalk.bc \
+	${GNUSTEP_LOCAL_ROOT}/Library/Frameworks/LanguageKitCodeGen.framework/Versions/0/Resources/MsgSendSmallInt.bc\
+	$BC
+
+# Optimise the bitcode
+echo Running optimiser...
+opt -f -O3 smalltalk.bc -o smalltalk.optimised.bc
+
+# Generate assembly
+echo Compiling bitcode...
+llc  smalltalk.optimised.bc
+
+# Assemble a .o file
+echo Assembling object file...
+clang -c smalltalk.optimised.s 
