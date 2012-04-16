26f62cff48273567c93ca385d55824acd6413434
thebeing <thebeing@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2012-04-16 13:03:11 +0000

    Start working on parameterized rules.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile@7651 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Languages/ParserKit/NSInvocation+pkextention.h
+++ Languages/ParserKit/NSInvocation+pkextention.h
@@ -2,4 +2,5 @@
 
 @interface NSInvocation (pkextension)
 -(id)returnValueAsObject;
+-(id)returnValueAsBool;
 @end
--- Languages/ParserKit/NSInvocation+pkextention.m
+++ Languages/ParserKit/NSInvocation+pkextention.m
@@ -1,8 +1,16 @@
 #include "NSInvocation+pkextention.h"
+#include <Foundation/NSValue.h>
 @implementation NSInvocation (pkextention)
 - (id) returnValueAsObject {
   id object;
   [self getReturnValue: &object];
   return object;
+}
+
+- (id)returnValueAsBool {
+	BOOL returnValue = NO;
+	[self getReturnValue: &returnValue];
+
+	return [NSNumber numberWithBool: returnValue];
 }
 @end
